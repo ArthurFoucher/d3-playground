35ef01c1c9c02320ba8ddbacff0acee7c8646248
ArthurFoucher <foucherarthur@gmail.com>
2019-07-29 17:12:05 +0200

    Create bar chart with d3 and scale

--- index.js
+++ index.js
@@ -1,52 +1,18 @@
-var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+const data = [4, 8, 15, 16, 23, 42];
 
-var svg = d3.select('svg'),
-  width = +svg.attr('width'),
-  height = +svg.attr('height'),
-  g = svg.append('g').attr('transform', 'translate(32,' + height / 2 + ')');
+const scale = d3
+  .scaleLinear()
+  .domain([0, d3.max(data)])
+  .range([0, 500]);
 
-function update(data) {
-  const t = g.transition().duration(750);
-  g.selectAll('text')
-    .data(data, d => d)
-    .join(
-      enter =>
-        enter
-          .append('text')
-          .attr('class', 'enter')
-          .attr('x', (d, i) => i * 32)
-          .attr('dy', -50)
-          .attr('opacity', 0)
-          .text(d => d)
-          .call(enter =>
-            enter
-              .transition(t)
-              .attr('dy', 0)
-              .attr('opacity', 1)
-          ),
-      update =>
-        update
-          .attr('class', 'update')
-          .call(enter => enter.transition(t).attr('x', (d, i) => i * 32)),
-
-      exit =>
-        exit.attr('class', 'exit').call(exit =>
-          exit
-            .transition()
-            .attr('dy', 50)
-            .style('opacity', 0)
-            .remove()
-        )
-    );
-}
-
-update(alphabet.split(''));
-
-d3.interval(function() {
-  update(
-    d3
-      .shuffle(alphabet.split(''))
-      .slice(0, Math.floor(Math.random() * 26))
-      .sort()
+d3.select('body')
+  .append('div')
+  .attr('class', 'chart')
+  .selectAll('div')
+  .data(data)
+  .join(enter =>
+    enter
+      .append('div')
+      .style('width', d => `${scale(d)}px`)
+      .text(d => d)
   );
-}, 1500);
