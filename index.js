9866bc5050dfef3bb7ff0a30511fb203dd319574
ArthurFoucher <foucherarthur@gmail.com>
2019-07-29 15:28:58 +0200

    Added animated circles

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-var chartData = [4, 8, 15, 16, 23, 42];
-
-d3.select('body')
-  .append('div')
-  .attr('class', 'chart')
-  .selectAll('div')
-  .data(chartData)
-  .enter()
-  .append((d, i) =>
-    i % 2 ? document.createElement('div') : document.createElement('span')
-  )
-  .style('width', d => `${d * 10}px`)
-  .text(d => d);
-
 const RADIUS = 25;
 const circleData = [{ x: 2 * RADIUS, y: RADIUS }, { x: 5 * RADIUS, y: RADIUS }];
 
-d3.select('body')
-  .append('svg')
-  .attr('class', 'circle')
-  .selectAll('circle')
-  .data(circleData)
-  .enter()
-  .append('circle')
-  .attr('cx', d => d.x)
-  .attr('cy', d => d.y)
-  .attr('fill', (d, i) => (i % 2 ? 'aquamarine' : 'deepskyblue'))
-  .attr('r', RADIUS);
+d3.select('body').append('svg');
+
+const update = () => {
+  const circle = d3
+    .select('svg')
+    .selectAll('circle')
+    .data(circleData);
+
+  circle
+    .exit()
+    .transition()
+    .attr('r', 0)
+    .remove();
+
+  const circleCreate = circle.enter().append('circle');
+
+  circleCreate
+    // @ts-ignore
+    .merge(circle)
+    .attr('cx', d => d.x)
+    .attr('cy', d => d.y)
+    .attr('fill', (d, i) => (i % 2 ? 'aquamarine' : 'deepskyblue'));
+
+  circleCreate
+    .attr('r', 0)
+    .transition()
+    .attr('r', RADIUS);
+};
+
+update();
+
+const id = setInterval(() => {
+  circleData.pop();
+  update();
+
+  if (circleData.length === 0) {
+    clearInterval(id);
+  }
+}, 1500);
