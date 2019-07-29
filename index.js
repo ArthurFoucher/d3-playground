c9d464ca2945d104ca9c9e92b6742e7f8a6bb806
ArthurFoucher <foucherarthur@gmail.com>
2019-07-29 14:30:19 +0200

    Draw bar chart

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var chartData = [4, 8, 15, 16, 23, 42];
+
 d3.select('body')
-  .style('color', 'white')
-  .style('background-color', 'black');
+  .append('div')
+  .attr('class', 'chart')
+  .selectAll('div')
+  .data(chartData)
+  .enter()
+  .append((d, i) =>
+    i % 2 ? document.createElement('div') : document.createElement('span')
+  )
+  .style('width', d => `${d * 10}px`)
+  .text(d => d);
