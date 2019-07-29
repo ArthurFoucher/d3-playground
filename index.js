a7fd1f87539ba44106fd4f9bb7904429af745969
ArthurFoucher <foucherarthur@gmail.com>
2019-07-29 13:53:28 +0200

    Method chhaining

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const div = document.createElement('div');
-div.innerHTML = 'Hello, world!';
-document.body.appendChild(div);
-
-const body = d3.select('body');
-const div2 = d3.select('body').append('div');
-div2.html('Hello, world!');
+d3.select('body')
+  .style('color', 'white')
+  .style('background-color', 'black');
